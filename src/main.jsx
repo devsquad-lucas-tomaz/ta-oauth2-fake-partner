123dd8da4421f583c6364ccbc1d5297a235d06c5
Lucas Tomaz <lucas.tomaz@devsquad.com>
2025-05-15 13:43:59 -0300

    add toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store/store';
+import { ToastContainer } from 'react-toastify';
 import App from './App.jsx'
 import './styles.css'
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
+          <ToastContainer />
         </BrowserRouter>
       </PersistGate>
     </Provider>
