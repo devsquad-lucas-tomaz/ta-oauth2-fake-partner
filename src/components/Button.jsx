c11486119c103bc0194163a2a0918f3896c3bd89
Lucas Tomaz <lucas.tomaz@devsquad.com>
2025-05-15 13:13:01 -0300

    Add support to loading state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,7 @@ export default function Button({
   href = '',
   children,
   disabled = false,
+  loading = false,
   className = '',
   ...rest
 }) {
@@ -47,8 +48,14 @@ export default function Button({
   // Button or anchor
   if (!href) {
     return (
-      <button className={classes} disabled={disabled} {...rest}>
-        {children}
+      <button className={classes} disabled={disabled || loading} {...rest}>
+        {loading && (
+          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        )}
+        <span>{children}</span>
       </button>
     );
   }
