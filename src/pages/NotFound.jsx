bedd62c05bb2f76457e85e00b7dd7ad55c73fe70
Lucas Tomaz <lucas.tomaz@devsquad.com>
2025-05-16 14:57:09 -0300

    Add 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,21 @@
+import { useNavigate } from 'react-router';
+import Button from '../components/Button';
+
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-red-600">404 - Page Not Found</h1>
-      <p className="mt-4 text-neutral-700">Sorry, the page you are looking for does not exist.</p>
-    </div>
+    <section>
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div className="mx-auto max-w-screen-sm text-center">
+                <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-secondary-400">404</h1>
+                <p className="mb-4 text-3xl tracking-tight font-bold text-neutral-900 md:text-4xl">Something's missing. 😔</p>
+                <p className="mb-4 text-lg font-light text-neutral-500">Sorry, we can't find that page.  </p>
+                <Button onClick={navigate('/')} className="my-4 mx-auto w-52">Back to Home</Button>
+                <img src="/404.gif" alt="404" className="rounded-3xl mx-auto w-3xs mt-10" />
+            </div>   
+        </div>
+    </section>
   );
 }
 
