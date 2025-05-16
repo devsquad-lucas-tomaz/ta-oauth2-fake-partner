dbbc0db1207805f3b9448f06f872eb49e5ae7908
Lucas Tomaz <lucas.tomaz@devsquad.com>
2025-05-16 14:59:12 -0300

    fix navigate

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -11,7 +11,7 @@ function NotFound() {
                 <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-secondary-400">404</h1>
                 <p className="mb-4 text-3xl tracking-tight font-bold text-neutral-900 md:text-4xl">Something's missing. 😔</p>
                 <p className="mb-4 text-lg font-light text-neutral-500">Sorry, we can't find that page.  </p>
-                <Button onClick={navigate('/')} className="my-4 mx-auto w-52">Back to Home</Button>
+                <Button onClick={() => navigate('/')} className="my-4 mx-auto w-52">Back to Home</Button>
                 <img src="/404.gif" alt="404" className="rounded-3xl mx-auto w-3xs mt-10" />
             </div>   
         </div>
